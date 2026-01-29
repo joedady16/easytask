2a8a49845b7b237f82d2252c031ab3cf235792d1
JoeS <joedady16@gmail.com>
2026-01-28 19:41:59 -0700

    finished users component and started tasks

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,26 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [RouterOutlet, Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-angular-app');
+  // protected readonly title = signal('first-angular-app');
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find(user => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
+
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, computed, Output, EventEmitter, output} from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+interface UserProps {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -10,14 +22,46 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
 
-  get userImagePath() {
-    return `./assets/users/${this.selectedUser.avatar}`;
+export class User {
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => `./assets/users/${this.selectedUser().avatar}`);
+
+  //=================================================
+  //Decorator approach:
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+
+  @Input({required: true}) user!: UserProps; //{ // <== Alternative approach to pass user object
+  //  id: string;
+  //  name: string;
+  //  avatar: string; 
+  // }; 
+
+  @Output() select = new EventEmitter<string>(); // <== Traditional output approach and adding <string> is a good practice but not required
+  // select = output<string>(); // <== New signal-based output approach
+  //=================================================
+   get imagePath() {
+    return `./assets/users/${this.user.avatar}`;
   }
 
+  //=================================================
+  //data container; signal approach:
+  // avatar = input.required<string>();
+  // name = input.required<string>(); 
+
+  // imagePath = computed(() => `./assets/users/${this.avatar}`);
+  //=================================================
+
+  // get userImagePath() {
+  //   return `./assets/users/${this.selectedUser.avatar}`;
+  // }
+
   onSelectUser() {
-    console.log('Clicked!');
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.user.id);
   }
 }
